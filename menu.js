0a1922d5913f7c29702e4757d9cfc08fc81d5fc8
Sami Samara <samisamara99@yahoo.com>
2022-06-18 17:31:23 -0500

    h3s

--- menu.js
+++ menu.js
@@ -2,3 +2,12 @@
 // create an object class for each item
 // each object pulls information from the menu options database
 // create a template html function and add each item to the current html
+
+// a snapshot is a picture of what the collection of data looks like in that period of time
+db.collection('drinks').get().then((snapshot) => {
+    // when we have the data
+    console.log(snapshot);
+}).catch(err => {
+    console.log(err);
+})
+
